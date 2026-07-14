ee82b051f6aa530ce20ba6f2a8a443573c0dbad4
jahdaganj02ki-sketch <jahdaganj02ki@gmail.com>
2026-07-14 15:11:48 +0000

    Fix GpuProbe: RenderMode lives in System.Windows.Interop

--- AgnesAIImageEdit/Services/GpuProbe.cs
+++ AgnesAIImageEdit/Services/GpuProbe.cs
@@ -38,7 +38,7 @@ namespace AgnesAIImageEdit.Services
 
             if (force || detected || lowTier)
             {
-                System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Media.RenderMode.SoftwareOnly;
+                System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
             }
         }
     }
